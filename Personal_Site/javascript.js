f7083917f3e7619a3a6925a052f4f4aa2f3690ad
Axelsend <denveraxelsen@gmail.com>
2024-10-15 19:18:44 -0500

    Add files via upload

---
+++ Personal_Site/javascript.js
@@ -0,0 +1,3 @@
+const userMessage = "Welcome to the Portfolio page of This Is: Denver Axelsen. Please see the GitHub link at the bottom of the page for more portfolio examples"
+
+alert(userMessage)
